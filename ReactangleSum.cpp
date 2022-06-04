1a1e1f2c05c57a394b60d3b9effbcd2d0e934178
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-06-04 11:58:27 +0530

    rivision done

--- ReactangleSum.cpp
+++ ReactangleSum.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-// ROW CHACHING
+// ROW CACHING
 class NumMatrix
 {
     vector<vector<int>> mat, sums;
@@ -42,5 +42,39 @@ public:
             sum += sums[i][col2 + 1] - sums[i][col1];
         }
         return sum;
+    }
+};
+
+// 2D CACHING
+class NumMatrix
+{
+    vector<vector<int>> mat, sums;
+
+public:
+    NumMatrix(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] += matrix[i - 1][j];
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+        mat = matrix;
+    }
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        int total = mat[row2][col2];
+        int extra = (row1 != 0 ? mat[row1 - 1][col2] : 0) + (col1 != 0 ? mat[row2][col1 - 1] : 0) - ((row1 != 0 && col1 != 0) ? mat[row1 - 1][col1 - 1] : 0);
+        return total - extra;
     }
 };
